5d3154f43542856e22a0ffa6119d68b149d3c2f3
Nerg <mas.ikhwanalhakim@gmail.com>
2023-06-12 14:50:41 +0700

    perbaikan footer

--- src/Features/Footer/footer.js
+++ src/Features/Footer/footer.js
@@ -4,11 +4,17 @@ import WaIcon from '../../Assets/wa-icon.svg';
 
 function Footer() {
   return (
-    <div className="sticky absolute bottom-0 w-full h-[100px] bg-white border-bordergray border-t">
-      <img src={LoakpostLogo} alt="Loakpost Logo" className="mt-2 ml-5 w-10 h-5" />
-      <div className="mt-1 ml-5 w-[60%] text-[10px]">Loak post is a Jatinangor based marketplace for secondhand items.</div> 
-      <div className="w-full flex border-bordergray border-t items-center">
-        <div className="ml-5 text-[15px] font-bold">Contact Us Through</div>
+    <div className="sticky absolute bottom-0 w-full h-[100px] bg-white border-bordergray border-t items-center">
+      <img src={LoakpostLogo} alt="Loakpost Logo" className="mt-2 ml-[3%] w-10 h-5" />
+      <div className="mt-1 ml-[3%] w-[60%] text-[10px]">Loak post is a Jatinangor based marketplace for secondhand items.</div> 
+      <div className="flex relative w-full h-[35px] items-center border-bordergray border-t">
+        <div className="justify-start w-full ml-[3%]">
+          <div className="font-bold text-[12px]">Contact Us Through</div>
+        </div>
+        <div className="justify-end w-full flex mr-[3%]">
+          <img src={IgIcon} alt="Instagram Icon" className="float-left" />
+          <img src={WaIcon} alt="Whatsapp Icon" className="float-left pl-2" />
+        </div>
       </div>
     </div>
   );
