7757b521c466eade52dd122e0a94a7ab3ee07abc
NergLite <mas.ikhwanalhakim@gmail.com>
2023-06-19 10:20:25 +0700

    penambahan pop up pada menu header

--- src/Features/Header/header.js
+++ src/Features/Header/header.js
@@ -1,9 +1,17 @@
+import React, { useState } from 'react';
 import LoakPostLogo from '../../Assets/loakpost-logo.svg';
 import SearchIcon from '../../Assets/search-icon.svg';
 import MenuIcon from '../../Assets/menu-icon.svg';
 import SellIcon from '../../Assets/sell-icon.svg';
 
 function Header() {
+
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
   return (
     <div className="flex relative w-full h-10 items-center">
       <div className="justify-start w-full ml-[3%]">
@@ -11,7 +19,18 @@ function Header() {
       </div>
       <div className="justify-end w-full flex mr-[3%]">
         <img src={SearchIcon} alt="Search Icon" className="float-left" />
-        <img src={MenuIcon} alt="Menu Icon" className="float-left pl-1" />
+        <img src={MenuIcon} alt="Menu Icon" className="float-left pl-1 cursor-pointer" onClick={toggleMenu} />
+        {menuOpen && (
+          <div className="top-[29px] z-50 mr-[50px] absolute bg-white w-[75px] h-[100px] rounded-l-[7px] rounded-br[7px] border border-bordergray flex flex-col items-end">
+            <div className="text-fontgray text-[9px] mr-[12px] my-[5px]">Home</div>
+            <div className="h-[1px] bg-bordergray w-[85%] mr-[7.5%]" />
+            <div className="text-fontgray text-[9px] mr-[12px] my-[5px]">Categories</div>
+            <div className="h-[1px] bg-bordergray w-[85%] mr-[7.5%]" />
+            <div className="text-fontgray text-[9px] mr-[12px] my-[5px]">About Us</div>
+            <div className="h-[1px] bg-bordergray w-[85%] mr-[7.5%]" />
+            <div className="text-fontgray text-[9px] mr-[12px] my-[5px]">FAQ</div>
+          </div>
+        )}
         <img src={SellIcon} alt="Sell Icon" className="float-left pl-1" />
       </div>
     </div>
