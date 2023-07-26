386f417462c66aef8463c139a0501281e0cd5d3d
NergLite <mas.ikhwanalhakim@gmail.com>
2023-07-26 14:13:46 +0700

    search bar dapat digunakan

--- src/Features/Header/header.js
+++ src/Features/Header/header.js
@@ -7,7 +7,8 @@ import SellIcon from '../../Assets/sell-icon.svg';
 
 function Header() {
   const [menuOpen, setMenuOpen] = useState(false);
- 
+  const [searchQuery, setSearchQuery] = useState('');
+
   const toggleMenu = () => {
     setMenuOpen(!menuOpen);
   };
@@ -30,16 +31,33 @@ function Header() {
     navigate('/sell');
   };
 
+  const handleSearchChange = (e) => {
+    setSearchQuery(e.target.value);
+    // You can add further logic here if needed (e.g., perform search operation).
+  };
+
   return (
-    <div className="flex relative w-full h-10 items-center">
+    <div className="flex relative w-full h-[40px] items-center">
       <div className="justify-start w-full ml-[3%]">
-        <img src={LoakPostLogo} alt="LoakPost Logo" className="w-10 h-5 cursor-pointer" onClick={handleHomeClick} />
+        <img
+          src={LoakPostLogo}
+          alt="LoakPost Logo"
+          className="w-10 h-5 cursor-pointer"
+          onClick={handleHomeClick}
+        />
       </div>
       <div className="justify-end w-full flex mr-[3%]">
-        <img src={SearchIcon} alt="Search Icon" className="float-left" />
+        <div className="h-[28px] w-full border-black border rounded-xl flex">
+          <img src={SearchIcon} alt="Search Icon" className="my-[3px] ml-[5px]" />
+          <input
+            value={searchQuery}
+            onChange={handleSearchChange}
+            className="ml-[5px] mt-[1px] h-[25px] w-[87%] text-[10px] outline-none"
+            placeholder="Cari barang bekas?"/>
+        </div>
         <img src={MenuIcon} alt="Menu Icon" className="float-left pl-1 cursor-pointer" onClick={toggleMenu} />
         {menuOpen && (
-          <div className="top-[29px] z-50 mr-[50px] absolute bg-white w-[75px] h-[75px] rounded-l-[7px] rounded-br[7px] border border-bordergray flex flex-col items-end">
+          <div className="top-[29px] z-50 mr-[50px] absolute bg-white w-[75px] h-[75px] rounded-l-[7px] rounded-br-[7px] border border-bordergray flex flex-col items-end">
             <div className="text-fontgray text-[9px] mr-[12px] my-[5px] cursor-pointer" onClick={handleHomeClick}>Home</div>
             <div className="h-[1px] bg-bordergray w-[85%] mr-[7.5%]" />
             <div className="text-fontgray text-[9px] mr-[12px] my-[5px] cursor-pointer" onClick={handleAboutUsClick}>About Us</div>
